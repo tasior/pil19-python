a13657d31f3fded991b4ae82c2246f6886cdc047
jgorka <jgorka@csod.com>
2024-06-29 23:57:24 +0200

    Refactor - main loop

--- lib-source/wifi.py
+++ lib-source/wifi.py
@@ -10,15 +10,22 @@ WLAN_MODE_IF = network.STA_IF
 
 class WiFi:
 
-    def __init__(self, callback = lambda: None, mode = network.STA_IF, retry_count = 20) -> None:
+    def __init__(self, mode = network.STA_IF, retry_count = 20) -> None:
         self.mode = mode
-        self.callback = callback
+        self.listeners = []
         self.retry_count = retry_count
         self.wlan = network.WLAN(self.mode)
         if mode == network.STA_IF:
             self.wlan.active(True)
             self.wlan.config(pm = 0xa11140)
     
+    def add_listener(self, listener):
+        self.listeners.append(listener)
+
+    def fire(self, event, data = None):
+        for listener in self.listeners:
+            listener(self, event, data)
+
     def scan(self):
         return self.wlan.scan()
     
@@ -27,81 +34,31 @@ class WiFi:
             self.wlan.config(ssid=ssid, password=password)
             self.wlan.active(True)
             while self.wlan.active() == False:
-                self.callback(self, EVENT_CONNECTING)
-            self.callback(self, EVENT_CONNECTED)
+                self.fire(EVENT_CONNECTING)
         else:
             retries = self.retry_count
             self.wlan.connect(ssid, password)
             while retries > 0 and self.wlan.status() != network.STAT_GOT_IP:
-                self.callback(self, EVENT_CONNECTING, retries)
+                self.fire(EVENT_CONNECTING, retries)
                 retries -= 1
                 time.sleep(1) 
 
-            self.callback(self, EVENT_CONNECTION_ERROR) if not self.is_connected() else self.callback(self, EVENT_CONNECTED)
+            if not self.is_connected():
+                self.fire(EVENT_CONNECTION_ERROR) 
+                return False
+        
+        self.fire(EVENT_CONNECTED)
+        return True
 
     def disconnect(self):
         if self.mode == WLAN_MODE_AP:
             self.wlan.deinit()
         else:
             self.wlan.disconnect()
-        self.callback(self, EVENT_DISCONNECT)
+        self.fire(self, EVENT_DISCONNECT)
 
     def is_connected(self):
         return self.wlan.status() == network.STAT_GOT_IP
-
-    def ip(self):
-        return self.wlan.ifconfig()[0]
-
-class WiFi_old:
-    
-    def __init__(self, cb, retries=10) -> None:
-        self.retries = retries
-        self.cb = cb
-        self.wlan = network.WLAN(network.STA_IF)
-        self.wlan.active(True)
-        self.wlan.config(pm = 0xa11140)
-
-    def scan(self):
-        self.wlan.scan()
-
-    def connect(self, ssid, password):
-        retries = self.retries
-        self.wlan.connect(ssid, password)
-        while retries > 0 and self.wlan.status() != network.STAT_GOT_IP:
-            retries -= 1
-            self.cb(EVENT_CONNECTING)
-            time.sleep(1) 
-
-        self.cb(EVENT_CONNECTION_ERROR) if not self.is_connected() else self.cb(EVENT_CONNECTED)
-
-    def disconnect(self):
-        self.wlan.disconnect()
-        self.cb(EVENT_DISCONNECT)
-
-    def is_connected(self):
-        return self.wlan.status() == network.STAT_GOT_IP
-
-    def ip(self):
-        return self.wlan.ifconfig()[0]
-
-class WiFi_AP:
-
-    def __init__(self, cb) -> None:
-        self.wlan = network.WLAN(network.AP_IF)
-        self.cb = cb
-
-    def up(self, ssid, password):
-        self.wlan.config(ssid=ssid, password=password)
-        self.wlan.active(True)
-
-        while self.wlan.active() == False:
-            self.cb('connecting')
-
-        self.cb('connected')
-
-    def down(self):
-        self.wlan.deinit()
-        self.cb('disconnected')
 
     def ip(self):
         return self.wlan.ifconfig()[0]
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import sys
+import uasyncio as asyncio
 from machine import soft_reset
-from config import read_config, MODE_ADMIN, MODE_APP
-from wifi import WiFi, WiFi_AP, WLAN_MODE_AP, WLAN_MODE_IF, EVENT_CONNECTING, EVENT_CONNECTED, EVENT_CONNECTION_ERROR, EVENT_DISCONNECT
+from time import sleep
+
+from config import read_config, MODE_ADMIN
+from wifi import WiFi, WLAN_MODE_AP, WLAN_MODE_IF, EVENT_CONNECTING, EVENT_CONNECTED, EVENT_CONNECTION_ERROR, EVENT_DISCONNECT
 # from config_server import config_server
 # from main_server import main_server
 
-def wlan_callback(wlan: WiFi, event, data = None):
+wlan: WiFi | None = None
+server = None
+cron = None
+
+def on_wlan(wlan: WiFi, event, data = None):
     print('[Wlan] Event: {} {}'.format(event, data))
     if event == EVENT_CONNECTED:
         print('[Wlan] Connected. IP: {}'.format(wlan.ip()))
 
-wlan = None
+async def main(wlan: WiFi, server, cron):
+    print('[Main] Initializing WLAN...')
+
+    if wlan.connect(config['wifi_ssid'], config['wifi_password']):
+        print('OK')
+    else:
+        raise RuntimeError('Cannot connect to wifi')
 
 try:
     print('[Main] Boot')
@@ -18,28 +31,19 @@ try:
     config = read_config()
     mode = config.get('mode', 'admin')
     print('OK')
-    wlan = WiFi(callback=wlan_callback, mode=WLAN_MODE_AP if config == MODE_ADMIN else WLAN_MODE_IF)
-    wlan.connect(config['wifi_ssid'], config['wifi_password'])
+    
+    wlan = WiFi(mode=WLAN_MODE_AP if config == MODE_ADMIN else WLAN_MODE_IF)
+    wlan.add_listener(on_wlan)
+
+    asyncio.run(main(wlan=wlan, server=None, cron=None))
 except Exception as e:
     print('[Main] Exception')
     sys.print_exception(e) # type: ignore
 finally:
-    if isinstance(wlan, WiFi) and wlan.is_connected():
+    print('[Main] Finally')
+    if wlan is not None and wlan.is_connected():
         print('[Main] Wlan disconnect')
         wlan.disconnect()
-
-
-# try:
-#     print('boot...')
-#     print('read config...', end='')
-#     config = read_config()
-#     mode = config.get('mode', 'config')
-#     print('OK')
-#     print(f'running {mode} mode')
-#     config_server(config, debug=False) if mode == 'config' else main_server(config)
-# except Exception as e:
-#     print('exception!!!')
-#     import sys
-#     sys.print_exception(e) # type: ignore
-#     print('rebooting')
-#     # soft_reset()
+    _ = asyncio.new_event_loop()
+    # sleep(5)
+    # soft_reset()
