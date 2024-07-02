457162ad59e94e8c1bf8d41c423ac384d0fdbada
jgorka <jgorka@csod.com>
2024-07-02 10:55:13 +0200

    Build tool

---
+++ tools/build.bat
@@ -0,0 +1,19 @@
+
+if exist ".\build" (
+    rmdir ".\build" /s /q
+)
+
+mkdir ".\build" 
+
+mpy-cross -march=armv6m .\lib-source\microdot.py -o .\lib.\microdot.mpy
+mpy-cross -march=armv6m .\lib-source\microdot_asyncio.py -o .\lib.\microdot_asyncio.mpy
+mpy-cross -march=armv6m .\lib-source\microdot_asyncio_websocket.py -o .\lib.\microdot_asyncio_websocket.mpy
+mpy-cross -march=armv6m .\lib-source\microdot_websocket.py -o .\lib.\microdot_websocket.mpy
+mpy-cross -march=armv6m .\lib-source\pil19.py -o .\lib.\pil19.mpy
+mpy-cross -march=armv6m .\lib-source\ssd1306.py -o .\lib.\ssd1306.mpy
+mpy-cross -march=armv6m .\lib-source\wifi.py -o .\lib.\wifi.mpy
+
+xcopy /s .\lib .\build\lib\
+xcopy /s .\web .\build\web\
+
+xcopy .\*.py .\build
